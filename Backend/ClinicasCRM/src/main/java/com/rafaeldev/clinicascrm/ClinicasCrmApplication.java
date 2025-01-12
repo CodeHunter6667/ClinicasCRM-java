53720c90059bcbdaaeb0036bd249690a9586dd8a
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-01-12 17:56:39 -0300

    commit inicial

---
+++ Backend/ClinicasCRM/src/main/java/com/rafaeldev/clinicascrm/ClinicasCrmApplication.java
@@ -0,0 +1,13 @@
+package com.rafaeldev.clinicascrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicasCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicasCrmApplication.class, args);
+	}
+
+}
---
+++ Backend/ClinicasCRM/src/test/java/com/rafaeldev/clinicascrm/ClinicasCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaeldev.clinicascrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicasCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
